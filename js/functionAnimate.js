ec4e10e2b426c235a651c91dd401c57780a57327
cristianangulonova <cristianangulonova@MacBook-Pro-de-Cristian.local>
2013-10-09 12:33:32 -0500

    Barrita de carga
    Muestra proceso de carga de la página...

---
+++ js/functionAnimate.js
@@ -0,0 +1,16 @@
+﻿$(window,document,undefined).ready(function(){
+var antes = (new Date()).getTime();
+var despues = (new Date()).getTime();
+tiempo = (antes-despues) / 1000;
+
+	$('.barrita').animate({width:35+'%'},tiempo,function(){
+		$('.barrita').animate({width: 100+'%'},tiempo,function(){
+		$('.barrita').fadeOut(1000);
+	});
+
+});
+
+
+
+});
+
